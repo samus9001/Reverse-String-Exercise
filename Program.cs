0e748a9e15bc7ee8cdc9112544bd436a40844db5
Samus9001 <sameer99@outlook.com>
2022-11-16 18:50:21 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, please type a word and I will type it backwards!");
+            string UserInput = Console.ReadLine();
+
+            char[] arr = UserInput.ToCharArray();
+            Array.Reverse(arr);
+
+            Console.WriteLine(new String(arr));
+        }
+    }
+}
