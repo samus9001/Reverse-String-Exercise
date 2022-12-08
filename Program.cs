f92a610a7324c8fc133b1b586073123f71530212
Samus9001 <sameer99@outlook.com>
2022-12-08 18:09:17 +0000

    Updated code with comments and created a method and if statement to compare strings and let the user know if a palindrome is used

--- Program.cs
+++ Program.cs
@@ -4,13 +4,24 @@
     {
         static void Main(string[] args)
         {
-
+            //asks the user a prompt
             Console.WriteLine("Hello, please type a word and I will type it backwards!");
+
+            //user inputs response to prompt
             string UserInput = Console.ReadLine();
 
+            //for loop runs through the user input  by looking at the length of the given string and prints the output of each letter backwards on one line
+            string reverseWord = "";
             for (int i = UserInput.Length - 1; i >= 0; i--)
             {
+                //method created to store the reverse word then output the contents of the method to the user
+                reverseWord += UserInput[i];
                 Console.Write(UserInput[i]);
+            }
+            //if statement checks if the user input string matches the reverse word method string and outputs this to the user if true
+            if (UserInput.Equals(reverseWord, StringComparison.OrdinalIgnoreCase)) 
+            {
+            Console.WriteLine("\nThis word is a palindrome!");
             }
         }
     }
