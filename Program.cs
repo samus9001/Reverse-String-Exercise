ebd7079ac6240e37219572d72073e211ea9da838
Samus9001 <sameer99@outlook.com>
2022-12-17 19:59:57 +0000

    Fixed variable name format and indentatiohn

--- Program.cs
+++ Program.cs
@@ -8,20 +8,20 @@
             Console.WriteLine("Hello, please type a word and I will type it backwards!");
 
             //user inputs response to prompt
-            string UserInput = Console.ReadLine();
+            string userInput = Console.ReadLine();
 
             //for loop runs through the user input  by looking at the length of the given string and prints the output of each letter backwards on one line
             string reverseWord = "";
-            for (int i = UserInput.Length - 1; i >= 0; i--)
+            for (int i = userInput.Length - 1; i >= 0; i--)
             {
                 //method created to store the reverse word then output the contents of the method to the user
-                reverseWord += UserInput[i];
-                Console.Write(UserInput[i]);
+                reverseWord += userInput[i];
+                Console.Write(userInput[i]);
             }
             //if statement checks if the user input string matches the reverse word method string and outputs this to the user if true
-            if (UserInput.Equals(reverseWord, StringComparison.OrdinalIgnoreCase)) 
+            if (userInput.Equals(reverseWord, StringComparison.OrdinalIgnoreCase))
             {
-            Console.WriteLine("\nThis word is a palindrome!");
+                Console.WriteLine("\nThis word is a palindrome!");
             }
         }
     }
