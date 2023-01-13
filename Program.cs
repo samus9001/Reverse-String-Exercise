bf051fa709272ec803303f6e7f77f600507af710
Samus9001 <sameer99@outlook.com>
2023-01-13 19:23:11 +0000

    moved reverseWord variable inside the for loop in order for it to reset on each iteration

--- Program.cs
+++ Program.cs
@@ -1,27 +1,36 @@
-﻿namespace ReverseString
+﻿using System.Diagnostics.Metrics;
+
+namespace ReverseString
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //asks the user a prompt
-            Console.WriteLine("Hello, please type a word and I will type it backwards!");
+            Console.WriteLine("Hello, please type a word and I will type it backwards!\n");
 
-            //user inputs response to prompt
-            string userInput = Console.ReadLine();
+            while (true)
+            {
+                string reverseWord = "";
+                //user inputs response to prompt
+                string userInput = Console.ReadLine();
 
-            //for loop runs through the user input  by looking at the length of the given string and prints the output of each letter backwards on one line
-            string reverseWord = "";
-            for (int i = userInput.Length - 1; i >= 0; i--)
-            {
-                //method created to store the reverse word then output the contents of the method to the user
-                reverseWord += userInput[i];
-                Console.Write(userInput[i]);
-            }
-            //if statement checks if the user input string matches the reverse word method string and outputs this to the user if true
-            if (userInput.Equals(reverseWord, StringComparison.OrdinalIgnoreCase))
-            {
-                Console.WriteLine("\nThis word is a palindrome!");
+                //runs through the user input by looking at the length of the given string
+                for (int i = userInput.Length - 1; i >= 0; i--)
+                {
+                    //method created to store the reverse word
+                    reverseWord += userInput[i];
+                    //prints the output of each letter in reverse order on one line
+                    Console.Write(userInput[i]);
+                }
+
+                Console.WriteLine("\n");
+
+                //checks if the user input string matches the reverse word method string and outputs this to the user if true
+                if (userInput.Equals(reverseWord, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("This word is a palindrome!\n");
+                }
             }
         }
     }
